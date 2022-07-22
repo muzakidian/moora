c88d50d23d01917ca1600867be1cc2bba88fcb2f
Argian13 <argianvarell12@gmail.com>
2022-07-22 13:43:01 +0700

    dashbrd

--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -1,38 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>SPK MOORA</title>
-	<!-- core:css -->
-	<link rel="stylesheet" href="<?php echo base_url() ?>assets/vendors/core/core.css">
-	<!-- endinject -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>SPK MOORA</title>
+  <!-- core:css -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/vendors/core/core.css">
+  <!-- endinject -->
   <!-- plugin css for this page -->
-	<link rel="stylesheet" href="<?php echo base_url() ?>assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css">
-	<!-- end plugin css for this page -->
-	<!-- inject:css -->
-	<link rel="stylesheet" href="<?php echo base_url() ?>assets/fonts/feather-font/css/iconfont.css">
-	<link rel="stylesheet" href="<?php echo base_url() ?>assets/vendors/flag-icon-css/css/flag-icon.min.css">
-	<!-- endinject -->
-  <!-- Layout styles -->  
-	<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/demo_1/style.css">
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css">
+  <!-- end plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/fonts/feather-font/css/iconfont.css">
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/vendors/flag-icon-css/css/flag-icon.min.css">
+  <!-- endinject -->
+  <!-- Layout styles -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/demo_1/style.css">
   <!-- End layout styles -->
   <link rel="shortcut icon" href="<?php echo base_url() ?>assets/images/favicon.png" />
+
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="author" content="Brex-Thee" />
+  <!-- description -->
+  <meta name="description" content="Brex is a highly creative, modern, visually stunning and Bootstrap responsive multipurpose agency and portfolio HTML5 template." />
+  <!-- keywords -->
+  <meta name="keywords" content="creative, modern, clean, bootstrap responsive, html5, css3, portfolio, blog, agency, templates, multipurpose, one page, corporate, start-up, studio, branding, designer, freelancer, carousel, parallax, photography, personal" />
+  <!-- favicon -->
+
+  <!-- animation -->
+  <link rel="stylesheet" href="assets/assets1/css/plugins/animate.css" />
+  <!-- Animated Headlines -->
+  <link rel="stylesheet" href="assets/assets1/css/plugins/animated-headlines.css">
+  <!-- bootstrap -->
+  <link rel="stylesheet" href="assets/assets1/css/bootstrap.min.css" />
+  <!-- font-awesome icon -->
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <!-- Dosis Font -->
+  <link href="https://fonts.googleapis.com/css?family=Dosis:200,300,400,500,600,700,800" rel="stylesheet">
+  <!-- magnific popup -->
+  <link rel="stylesheet" href="assets/assets1/css/plugins/magnific-popup.css" />
+  <!-- Main Style -->
+  <link rel="stylesheet" href="assets/assets1/css/cssnew.css" />
 </head>
+
 <body class="sidebar-dark">
-	<div class="main-wrapper">
+  <div class="main-wrapper">
 
-		<!-- partial:partials/_sidebar.html -->
+    <!-- partial:partials/_sidebar.html -->
 
-		<!-- partial -->
-	
-		<div class="page-wrapper">	
-			<!-- partial:partials/_navbar.html -->
+    <!-- partial -->
 
-			<!-- partial -->
+    <div class="page-wrapper">
+      <!-- partial:partials/_navbar.html -->
 
-			<div class="page-content">
+      <!-- partial -->
+
+      <div class="page-content">
 
         <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
           <div>
@@ -57,17 +85,76 @@
             </button>
           </div>
         </div>
-			</div>
+        <br />
 
-			<!-- partial:partials/_footer.html -->
+        <div class="card">
+          <div class="card-body ">
 
-			<!-- partial -->		
-		</div>
-	</div>
+            <body>
+              <!-- ======== start header ======== -->
+              <header>
 
-	<!-- core:js -->
-	<script src="<?php echo base_url() ?>assets/vendors/core/core.js"></script>
-	<!-- endinject -->
+              </header>
+              <!-- ======== End header ======== -->
+
+              <!-- ======== Start Slider ======== -->
+              <section class="slider" id="slider">
+                <div class="slider-content text-left">
+          
+                  <h3 class="cd-headline clip is-full-width">
+                    <span class="cd-words-wrapper">
+                        <b class="is-visible">Sistem Pendukung Keputusan</b><br/>
+                        <b>Metode Multi Objective Optimization by Ratio Analysis</b>
+                        
+                    </span>
+                </h3>
+
+                  <p>Metode MOORA memiliki tingkat selektifitas yang baik karena dapat
+                    menentukan tujuan dari kriteria yang bertentangan.
+                    <br>
+                    Dimana kriteria dapat bernilai menguntungkan (benefit) atau yang tidak
+                    menguntungkan (cost).
+                  </p>
+
+                  <div class="buttons">
+                    <a href="<?php echo base_url() ?>welcome/dashboard" class="btn-1">Get Started</a>
+
+                  </div>
+
+                </div>
+              </section>
+              <div class="card           container-md">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="<?php echo base_url() ?>Kriteria/kriteria" class="btn btn-primary">Halaman Data Kriteria</a>
+                  
+                </div>
+              </div>
+<br/>
+              <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="<?php echo base_url() ?>Alternatif/alternatif" class="btn btn-">Halaman Data Alternatif</a>
+                </div>
+              </div>
+          </div>
+        </div>
+
+      </div>
+
+      <!-- partial:partials/_footer.html -->
+
+      <!-- partial -->
+    </div>
+  </div>
+
+  <!-- core:js -->
+  <script src="<?php echo base_url() ?>assets/vendors/core/core.js"></script>
+  <!-- endinject -->
   <!-- plugin js for this page -->
   <script src="<?php echo base_url() ?>assets/vendors/chartjs/Chart.min.js"></script>
   <script src="<?php echo base_url() ?>assets/vendors/jquery.flot/jquery.flot.js"></script>
@@ -75,14 +162,40 @@
   <script src="<?php echo base_url() ?>assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
   <script src="<?php echo base_url() ?>assets/vendors/apexcharts/apexcharts.min.js"></script>
   <script src="<?php echo base_url() ?>assets/vendors/progressbar.js/progressbar.min.js"></script>
-	<!-- end plugin js for this page -->
-	<!-- inject:js -->
-	<script src="<?php echo base_url() ?>assets/vendors/feather-icons/feather.min.js"></script>
-	<script src="<?php echo base_url() ?>assets/js/template.js"></script>
-	<!-- endinject -->
+  <!-- end plugin js for this page -->
+  <!-- inject:js -->
+  <script src="<?php echo base_url() ?>assets/vendors/feather-icons/feather.min.js"></script>
+  <script src="<?php echo base_url() ?>assets/js/template.js"></script>
+  <!-- endinject -->
   <!-- custom js for this page -->
   <script src="<?php echo base_url() ?>assets/js/dashboard.js"></script>
   <script src="<?php echo base_url() ?>assets/js/datepicker.js"></script>
-	<!-- end custom js for this page -->
+  <!-- end custom js for this page -->
+
+
+  <script src="assets/assets1/js/plugins/jquery-3.3.1.min.js"></script>
+  <!-- Bootstrap 4.1.3 -->
+  <script src="assets/assets1/js/bootstrap.min.js"></script>
+  <script src="js/plugins/popper.min.js"></script>
+  <!-- Animated Headlines -->
+  <script src="assets/assets1/js/plugins/animated-headlines.js"></script>
+  <!-- Nav jQuery-->
+  <script src="assets/assets1/js/plugins/nav.jquery.js"></script>
+  <!-- Nav jQuery-->
+  <script src="assets/assets1/js/plugins/jquery.stellar.js"></script>
+  <!-- Isotope -->
+  <script src="assets/assets1/js/plugins/isotope.pkgd.min.js"></script>
+  <!-- Image Loaded -->
+  <script src="assets/assets1/js/plugins/imagesloaded.pkgd.min.js"></script>
+  <!-- Magnific Popup -->
+  <script src="assets/assets1/js/plugins/magnific-popup.min.js"></script>
+  <!-- Smooth-Scroll -->
+  <script src="assets/assets1/js/smooth-scroll.js"></script>
+  <!-- Counter Up -->
+  <script src="assets/assets1/js/plugins/jquery.waypoints.min.js"></script>
+  <script src="assets/assets1/js/plugins/jquery.counterup.min.js"></script>
+  <!-- Main JS -->
+  <script src="assets/assets1/js/main.js"></script>
 </body>
-</html>    
+
+</html>
